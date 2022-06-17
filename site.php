fd234cc878fd6c6b829de02964b57a4f06f2fa26
epyion <98820600+epyion@users.noreply.github.com>
2022-06-17 15:31:24 +0200

    pull

--- site.php
+++ site.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPMincho&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/46b886e8d1.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="site.css">
     <title>Document</title>
 </head>
 <body>
     <div class="header">PROJECT</div>
+    <div class="bann"></div>
+    <h1>Premier Project</h1>
+    <div class="grid-container">
+        <div class="maquette">maquette</div>
+        <div class="site">site</div>
+        <div class="github"><i class="fa-brands fa-github"></i></div>
+    </div>
+
+
     
 </body>
 </html>
